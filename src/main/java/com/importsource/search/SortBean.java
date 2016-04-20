c224bce8cff99601d8ec306f774b1ca6875741b5
Hezf <Hezf@Hezf-PC>
2016-04-20 17:31:13 +0800

    FIRST

---
+++ src/main/java/com/importsource/search/SortBean.java
@@ -0,0 +1,24 @@
+package com.importsource.search;  
+
+/**
+ * 
+ * @author Hezf
+ *
+ */
+public class SortBean { 
+  private String id; 
+  private int times; 
+    
+  public String getId() { 
+    return id; 
+  } 
+  public void setId(String id) { 
+    this.id = id; 
+  } 
+  public int getTimes() { 
+    return times; 
+  } 
+  public void setTimes(int times) { 
+    this.times = times; 
+  } 
+} 
---
+++ src/main/java/com/importsource/search/Test.java
@@ -0,0 +1,29 @@
+package com.importsource.search;  
+  
+import java.util.List; 
+  
+/**
+ * 测试demo
+ * @author Hezf
+ *
+ */
+public class Test { 
+  public static void main(String[] args) { 
+    SerachBase serachBase = SerachBase.getSerachBase(); 
+    serachBase.add("1", "你好！", "你好！"); 
+    serachBase.add("2", "你好！我是张三。", "你好！我是张三。"); 
+    serachBase.add("3", "今天的天气挺好的。", "今天的天气挺好的。"); 
+    serachBase.add("4", "你是谁？", "你是谁？"); 
+    serachBase.add("5", "高数这门学科很难", "高数确实很难。"); 
+    serachBase.add("6", "测试", "上面的只是测试"); 
+    String ids = serachBase.getIds("你的高数"); 
+    System.out.println(ids); 
+    List<Object> objs = serachBase.getObjects(ids); 
+    if (objs != null) { 
+      for (Object obj : objs) { 
+        System.out.println((String) obj); 
+      } 
+    } 
+  } 
+  
+} 
